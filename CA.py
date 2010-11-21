1446f05ac5e99f5a9356a72de43ef65a9680170e
Felix Bondarenko (@home) <Felix Bondarenko (@home)>
2010-11-21 16:49:11 +0100

    started class vonNeumann( CA )

--- CA.py
+++ CA.py
@@ -68,7 +68,6 @@ class CA():
         # - a new line with info about the conf starts with '#'
         # - a new line of the conf starts with '##'
         # - the last line that is going to be interpreted starts with '###'
-#        try:
             with open( filename, 'w' ) as f:
                 # info
                 f.write( "#title=" + self.getTitle() + "\n" )
@@ -92,8 +91,6 @@ class CA():
                 # end-tag
                 f.write( "###\n" )
                 f.close()
-#        except:
-#            print "there was a problem with exporting"
 
     def getConf( self ):
         return self.currConf
@@ -154,7 +151,6 @@ class CA():
         #### WERE USING XASIM-EXPORT-STYLE ####
         # don't use this function but importConf()
         
-#        try:
             with open( filename, 'r' ) as f:
                 for line in f:
                     line = line[0:-1]
@@ -218,8 +214,6 @@ class CA():
             f.close()
             return retVal
 
-#        except:
-#            print "there was an error while importing from file", filename
 
     def loopFunc( self ):
         print "function loopFunc() not implemented yet"
@@ -235,14 +229,11 @@ class CA():
             self.sizeY = sizeY
         self.size = sizeX,sizeY
         if self.getDim() == 1:
-#            self.currConf = np.zeros( sizeX, int )
-#            self.nextConf = np.zeros( sizeY, int )
             self.currConf = np.zeros( (sizeX,1), int )
             self.nextConf = np.zeros( (sizeX,1), int )
         elif self.getDim() == 2:
             self.currConf = np.zeros( self.size, int )
             self.nextConf = np.zeros( self.size, int )
-#        print sizeX, sizeY
         
     def setConf( self, conf ):
         if conf.size != self.currConf.size:
@@ -252,9 +243,6 @@ class CA():
                 self.resize( conf[0].size, conf[1].size )
         self.currConf = conf.copy()
         self.nextConf = conf.copy()
-
-    def step( self ):
-        pass
 
 
 ########## binrule ##############
@@ -273,21 +261,15 @@ class binRule( CA ):
         self.size = ( sizeX, sizeY )
 
         if initConf == self.INIT_ZERO:
-#            self.currConf = np.zeros( (sizeX), int )
-#            self.nextConf = np.zeros( (sizeX), int )
             self.currConf = np.zeros( (sizeX, 1), int )
             self.nextConf = np.zeros( (sizeX, 1), int )
         elif initConf == self.INIT_ONES:
-#            self.currConf = np.ones( (sizeX), int )
-#            self.nextConf = np.ones( (sizeX), int )
             self.currConf = np.ones( (sizeX, 1), int )
             self.nextConf = np.ones( (sizeX, 1), int )
         elif initConf == self.INIT_RAND:
-#            self.currConf = np.ones( (sizeX), int )
             self.currConf = np.zeros( (sizeX, 1), int )
             self.nextConf = np.zeros( (sizeX, 1), int )
             for i in range( sizeX ):
-#                self.currConf[i] = random.randint( 0, 1 )
                 self.currConf[i,0] = random.randint( 0, 1 )
             self.nextConf = self.currConf.copy()
         elif initConf == INIT_FILE and filename != "":
@@ -320,7 +302,6 @@ class binRule( CA ):
         pass
     
     def step( self ):
-        CA.step( self )
         self.updateAllCellsWeaveInline()
 
     def updateAllCellsPy( self ):
@@ -570,10 +551,236 @@ class ballPile( sandPile ):
     def getTitle( self ):
         return "BallPile"
 
+
+########## von Neumann ##########
+class vonNeumann ( CA ):
+    palette = []
+    
+    def __init__( self, sizeX, sizeY, filename="" ):
+        self.size = self.sizeX, self.sizeY = sizeX, sizeY
+
+        self.currConf = np.zeros( (sizeX, sizeY), int )
+        self.nextConf = np.zeros( (sizeX, sizeY), int )
+        if filename != "":
+            self.importConf( filename )
+
+        for imgFile in ( "C00.jpg", "C01.jpg", "C10.jpg", "C11.jpg", 
+                         "S000.jpg", "S00.jpg", "S01.jpg", "S0.jpg", 
+                         "S10.jpg", "S11.jpg", "S1.jpg", "S.jpg", 
+                         "T000.jpg", "T001.jpg", "T010.jpg", "T011.jpg", 
+                         "T020.jpg", "T021.jpg", "T030.jpg", "T031.jpg", 
+                         "T100.jpg", "T101.jpg", "T110.jpg", "T111.jpg", 
+                         "T120.jpg", "T121.jpg", "T130.jpg", "T131.jpg", 
+                         "U.jpg" ):
+            img = pygame.image.load( filename ).convert()
+            self.palette.append( img )
+
+   
+    def getDim( self ):
+        return 2
+
+    def getTitle( self ):
+        return "vonNeumann"
+
+    def loopFunc( self ):
+        self.step()
+
+    def step( self ):
+        self.updateAllCellsWeaveInline()
+
+#  
+# All states are encoded in a bitmask:
+#
+#    <--MSB     20                  10                           LSB
+#  ...... 0 0 0 0 0 0 0 0 0 0 0 0 0 X X u a1 a0 eps s2 s1 s0  e1 e0
+#                                   | | | |  |  |    |  |  |  |  |-> current e
+#  XX = 00 -> U            <--------| | | |  |  |    |  |  |  |----> next e
+#  XX = 01 -> C            <----------| | |  |  |    |  |  |     
+#  XX = 10 -> S                         | |  |  |    |  |  |-------> lsb on S
+#  XX = 11 -> T                         | |  |  |    |  |----------> ...
+#                                       | |  |  |    |-------------> msb on S
+#  S{} is encoded as SMASK_111          | |  |  |
+#                                       | |  |  |------------------> excited
+#                                       | |  |---------------------> direction
+#                                       | |------------------------> direction
+#                                       |--------------------------> special
+#
+
+    def updateAllCellsWeaveInline( self ):
+        vonNeumannCode = """
+""";
+}
+
+
+
+
+#include <stdlib.h>
+
+#line 1 "CA.py"
+#define U              0
+#define CMASK        512  // 1 << 9
+#define SMASK       1024  // 2 << 9
+#define TMASK       1536  // 3 << 9
+#define CSTATEMASK     3  // 1 + 2
+#define SSTATEMASK    28  // 4 + 8 + 16
+#define TSTATEMASK   480  // 32 + 64 + 128 + 256
+#define e0     1
+#define e1     2
+#define s0     4
+#define s1     8
+#define s2    16
+#define s     28
+#define eps   32
+#define a0    64
+#define a1   128
+#define a    192
+#define u    256
+
+#define U(x) (x == 0)
+#define C(x) (x & CMASK)
+#define S(x) (x & SMASK)
+#define T(x) (x & TMASK)
+
+int code() {
+  int i, j, k, l;
+  int neighState[4];
+  int state;
+  for ( i = 1; i < sizeX-1; i++ ) {
+    for ( j = 1; j < sizeY-1; j++ ) {
+      state = cconf( i, j );
+      nbs[0] = cconf( i+1, j );
+      nbs[1] = cconf( i, j-1 );
+      nbs[2] = cconf( i-1, j );
+      nbs[3] = cconf( i, j+1 );
+      if ( T(state) ) { // transmission state
+	// transisition rule (T.1):
+	for ( k = 0; k < 4; k++ ) {
+	  if ( T(nbs[k]) && ( abs(k-((nbs[k]&a)>>6)) == 2) 
+	       && (nbs[k]&u != state&u) && (nbs[k]&eps)  ) {
+	    // (T.1)(alpha)
+	    nconf( i, j ) = U;
+	    break;
+	  }
+	}
+	if ( k == 4 ) {
+	  // (T.1)(beta)
+	  for ( k = 0; k < 4; k++ ) {
+	    if ( T(nbs[k]) && (abs(((nbs[k]&a)>>6)-((state&a)>>6)) != 2) 
+		 && (nbs[k]&u == state&u ) && (nbs[k]&eps) ) {
+	      // (T.1)(beta)(a)
+	      nconf( i, j ) = state | eps;
+	      break;
+	    }
+	    if ( C(nbs[k]) && (nbs[k]&e0) && (abs(k-((state&a)>>6)) != 2) ) {
+	      // (T.1)(beta)(b)
+	      nconf( i, j ) = state | eps;
+	      break;
+	    }
+	  }
+	}
+	if ( k == 4 ) {
+	  // (T.1)(gamme)
+	  nconf( i, j ) = TMASK | u | a;
+	}
+      } 
+
+      else if ( C(state) ) { // confluent state
+	
+      } 
+
+      else if ( U(state) ) {  // unexcitable state
+	// transition rule (T.3)
+	for ( k = 0; k < 4; k++ ) {
+	  if ( T(nbs[k]) && (abs(k-(nbs[k]&a)>>6) == 2) && (nbs[k]&eps) ) {
+	    // (T.3)(alpha)
+	    nconf( i, j ) = SMASK | s0 | s1 | s2;
+	    break;
+	  }
+        }
+	// (T.3)(beta)
+	// doesn' change the state
+      } 
+      
+      else if ( S(state) ) { // sensitized state
+	// transition rule (T.4)
+	l = state&(s1|s0);
+	for ( k = 0; k < 4; k++ ) {
+	  if ( T(nbs[k]) && (abs(k-(nbs[k]&a)>>6) == 2) && (nbs[k]&eps) ) {
+	    // (T.4)(alpha)
+	    nconf( i, j ) = SMASK | l<<1 + s0;
+	    break;	    
+	  }
+	}
+	if ( k == 4 ) {
+	  // (T.4)(beta)
+	  nconf( i, j ) = SMASK | l<<1;
+	}
+      } 
+
+      else  {
+	// error!
+      }
+
+    }
+  }
+
+     
+
+
+
+      
+
+
+
+
+        cconf = self.currConf
+        nconf = self.nextConf
+        sizeX = self.sizeX
+        sizeY = self.sizeY
+# try to reduce varpreparation by using a datastructure with size, all statemasks
+# etc. and access it in the weavecode as array. 
+        weave.inline( vonNeumannCode, [ 'cconf', 'nconf', 'sizeX', 'rule'],
+                      type_converters = converters.blitz,
+                      compiler = 'gcc' )
+        self.currConf = self.nextConf.copy()
+
+        
+        C00 = 0
+        C00 = 1
+        C01 = 2
+        C10 = 3
+        C11 = 4
+        S000 = 5
+        S00 = 6
+        S01 = 7
+        S0 = 8
+        S10 = 9
+        S11 = 10
+        S1 = 11
+        S = 12
+        T000 = 13
+        T001 = 14 
+        T010 = 15
+        T011 = 16
+        T020 = 17
+        T021 = 18
+        T030 = 19
+        T031 = 20
+        T100 = 21
+        T101 = 22
+        T110 = 23
+        T111 = 24
+        T120 = 25
+        T121 = 26
+        T130 = 27
+        T131 = 28
+        U = 29
+
+
+
 if __name__ == "__main__":
     sizeX, sizeY = 10, 10
     pygame.init()
     screen = pygame.display.set_mode( (sizeX,sizeY), 0, 8 )
     surf = pygame.surface.Surface( (sizeX, sizeY ), 0, 8 )
     ca = ballPile( 10, 10 )
-
