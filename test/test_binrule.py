ede8dd5006dc80cf60b2bac90dd386cf9446baca
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-07-23 23:11:51 +0200

    added test case for binrule.

---
+++ test/test_binrule.py
@@ -0,0 +1,26 @@
+import CA
+import numpy as np
+from random import randrange
+from testutil import assert_arrays_equal
+
+class TestBinRule:
+    def test_compare_weave_pure(self, ruleNum, random=True):
+        if random:
+            br = CA.binRule(110, randrange(10,30), 1, CA.binRule.INIT_RAND)
+        else:
+            content = np.array([1,1,1,1,1,0,1,0,1,1,0,0,0,1,1,0,1,0,0,0,1,0,0,0])
+            br = CA.binRule(110, len(content), 1, CA.binRule.INIT_ZERO)
+            br.setConf(content)
+        br2 = CA.binRule(110, len(br.getConf()), 1, CA.binRule.INIT_ZERO)
+        br2.setConf(br.getConf())
+        assert_arrays_equal(br.getConf(), br2.getConf())
+        for i in range(5):
+            print "step", i
+            br.updateAllCellsWeaveInline()
+            br2.updateAllCellsPy()
+            assert_arrays_equal(br.getConf(), br2.getConf())
+
+def pytest_generate_tests(metafunc):
+    if "ruleNum" in metafunc.funcargnames:
+        for i in range(128):
+            metafunc.addcall(funcargs=dict(ruleNum=i))
---
+++ test/testutil.py
@@ -0,0 +1,30 @@
+def assert_arrays_equal(arr1, arr2):
+    assert len(arr1) == len(arr2), "array lengths don't match"
+    equal = True
+    l1, mid, l2 = "arr1 ", "     ", "arr2 "
+    for i in range(len(arr1)):
+        if arr1[i] != arr2[i]:
+            equal = False
+
+        l1p = " % 2d" % arr1[i]
+        l2p = " % 2d" % arr2[i]
+        l1 += l1p
+        l2 += l2p
+        if len(l1) > len(l2):
+            l2 += " " * (len(l2) - len(l1))
+        elif len(l1) < len(l2):
+            l1 += " " * (len(l1) - len(l2))
+        if arr1[i] == arr2[i]:
+            mid += " " * len(l1p)
+        else:
+            mid += "  !" + " " * (max(len(l1p), len(l2p)) - 3)
+
+    if not equal:
+        print l1
+        print mid
+        print l2
+    else:
+        print l1
+
+    assert equal, "array content does not match"
+
