155ea2bdbfe1f44e89f68ee3d1b49dd320f7046c
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-08-11 14:09:50 +0200

    move code, target into Visitor class

--- CA_gen.py
+++ CA_gen.py
@@ -32,15 +32,27 @@ functions that take part in doing everything.
 from scipy import weave
 
 class WeaveStepFuncVisitor(object):
-    def visit(self, code):
+    def __init__(self):
+        self.code = None
+        self.target = None
+
+    def bind(self, code):
+        assert self.code is None, "%r is already bound to %r" % (self, self.code)
+        self.code = code
+
+    def set_target(self, target):
+        assert self.target is None, "%r already targets %r" % (self, self.target)
+        self.target = target
+
+    def visit(self):
         """add code snippets to the code object."""
         pass
 
-    def init_once(self, code, target):
+    def init_once(self):
         """initialize data on the target."""
         pass
 
-    def new_config(self, code, target):
+    def new_config(self):
         """check and sanitize a new config."""
         pass
 
@@ -61,19 +73,19 @@ class StateAccessor(WeaveStepFuncVisitor):
         example: nconf(pos, 0)"""
         return ""
 
-    def write_to(self, target, pos, value):
+    def write_to(self, pos, value):
         """directly write to the config supplied by target at pos."""
 
-    def read_from(self, target, pos):
+    def read_from(self, pos):
         """directly read from the config supplied by target at pos."""
         return 0
 
-    def get_size(self, code, dimension=0):
+    def get_size(self, dimension=0):
         """generate a code bit to get the size of the config space in
         the specified dimension"""
         return "size"
 
-    def get_size_of(self, target, dimension=0):
+    def get_size_of(self, dimension=0):
         """get the size of the config space in the specified dimension."""
         return 0
 
@@ -110,7 +122,7 @@ class BorderHandler(WeaveStepFuncVisitor):
 class WeaveStepFunc(object):
     """The WeaveStepFunc will compose different parts into a functioning
     step function."""
-    def __init__(self, loop, accessor, neighbourhood, extra_code=[]):
+    def __init__(self, loop, accessor, neighbourhood, extra_code=[], target=None):
         """create a weave-based step function from the specified parts.
 
         loop          -  a CellLoop, that adds a loop at loop_begin
@@ -135,7 +147,19 @@ class WeaveStepFunc(object):
         self.visitors = [self.acc, self.neigh, self.loop] + extra_code
 
         for code in self.visitors:
-            code.visit(self)
+            code.bind(self)
+
+        self.target = target
+        if self.target is not None:
+            for code in self.visitors:
+                code.set_target(self.target)
+
+        for code in self.visitors:
+            code.visit()
+
+    def set_target(self, target):
+        assert self.target is None, "%r already targets %r" % (self, self.target)
+        self.target = target
 
     def add_code(self, hook, code):
         self.code[hook].append(code)
@@ -146,17 +170,17 @@ class WeaveStepFunc(object):
             code_bits.extend(self.code[section])
         self.code_text = "\n".join(code_bits)
 
-    def inline(self, target):
+    def inline(self):
         weave.inline( self.code_text,
-            local_dict=dict((k, getattr(target, k)) for k in self.attrs))
+            local_dict=dict((k, getattr(self.target, k)) for k in self.attrs))
 
-    def new_conf(self, target):
+    def new_conf(self):
         for code in self.visitors:
-            code.new_conf(target)
+            code.new_conf(self.target)
 
-    def init_once(self, target):
+    def init_once(self):
         for code in self.visitors:
-            code.init_once(target
+            code.init_once()
 
 class LinearStateAccessor(StateAccessor):
     def read_access(self, pos):
@@ -165,17 +189,17 @@ class LinearStateAccessor(StateAccessor):
     def write_access(self, pos):
         return "nconf(%s, 0)" % pos
 
-    def visit(self, code):
-        code.add_code("localvars",
+    def visit(self):
+        self.code.add_code("localvars",
                 """int result;""")
-        code.add_code("post_compute",
-                self.write_access(code.loop.get_position()) + " = result")
+        self.code.add_code("post_compute",
+                self.write_access(self.code.loop.get_position()) + " = result")
 
-    def read_from(self, target, pos):
-        return target.currConf[pos]
+    def read_from(self, pos):
+        return self.target.currConf[pos]
 
-    def write_to(self, target, pos, value):
-        target.currConf[pos] = value
+    def write_to(self, pos, value):
+        self.target.currConf[pos] = value
 
 class LinearCellLoop(CellLoop):
     def get_position(self, offset=0):
@@ -184,23 +208,24 @@ class LinearCellLoop(CellLoop):
         else:
             return "i + %d" % (offset)
 
-    def visit(self, code):
-        code.add_code("loop_begin",
+    def visit(self):
+        self.code.add_code("loop_begin",
                 """for(int i=1; i < sizeX-1; i++) {""")
-        code.add_code("loop_end",
+        self.code.add_code("loop_end",
                 """}""")
 
 class LinearNeighbourhood(Neighbourhood):
     def __init__(self, names, offsets):
+        super(Neighbourhood, self).__init__()
         self.names = tuple(names)
         self.offsets = tuple(offsets)
         assert len(self.names) == len(self.offsets)
 
-    def visit(self, code):
+    def visit(self):
         for name, offset in zip(self.names, self.offsets):
-            code.add_code("pre_compute",
+            self.code.add_code("pre_compute",
                 "%s = %s" % (name,
-                             code.acc.read_access(code.loop.get_position(offset))))
+                             self.code.acc.read_access(self.code.loop.get_position(offset))))
 
     def neighbourhood_cells(self):
         return self.names
@@ -209,17 +234,17 @@ class LinearNeighbourhood(Neighbourhood):
         return min(self.offsets), max(self.offsets)
 
 class LinearBorderCopier(BorderHandler):
-    def visit(self, code):
-        code.add_code("after_step",
-                code.acc.write_access("0") + " = " + code.acc.write_access("sizeX - 2") + ";\n" +
-                code.acc.write_access("sizeX - 1") + " = " + code.acc.write_access("1") + ";")
+    def visit(self):
+        self.code.add_code("after_step",
+                self.code.acc.write_access("0") + " = " + self.code.acc.write_access("sizeX - 2") + ";\n" +
+                self.code.acc.write_access("sizeX - 1") + " = " + self.code.acc.write_access("1") + ";")
 
-    def new_config(self, code, target):
-        left = code.acc.read_from(target, 1)
-        right = code.acc.read_from(target, code.acc.get_size_of(target, 0) - 1)
+    def new_config(self):
+        left = self.code.acc.read_from(self.target, 1)
+        right = self.code.acc.read_from(self.target, self.code.acc.get_size_of(self.target, 0) - 1)
 
-        code.acc.write_to(target, 0, right)
-        code.acc.write_to(target, code.acc.get_size_of(target, 0) - 2, left)
+        self.code.acc.write_to(self.target, 0, right)
+        self.code.acc.write_to(self.target, self.code.acc.get_size_of(self.target, 0) - 2, left)
 
 def test():
     binRuleTestCode = WeaveStepFunc(
