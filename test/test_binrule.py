0af36c9e8d5b7d860d45cf2c0678e6723d333f9a
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-08-21 01:22:00 +0200

    whoops. actually test different binRule numbers.

--- test/test_binrule.py
+++ test/test_binrule.py
@@ -7,12 +7,12 @@ class TestBinRule:
     def test_compare_weave_pure(self, ruleNum, random=True):
         """compare the weave version of binRule with the pure python one"""
         if random:
-            br = CA.binRule(110, randrange(10,30), 1, CA.binRule.INIT_RAND)
+            br = CA.binRule(ruleNum, randrange(10,30), 1, CA.binRule.INIT_RAND)
         else:
             content = np.array([1,1,1,1,1,0,1,0,1,1,0,0,0,1,1,0,1,0,0,0,1,0,0,0])
-            br = CA.binRule(110, len(content), 1, CA.binRule.INIT_ZERO)
+            br = CA.binRule(ruleNum, len(content), 1, CA.binRule.INIT_ZERO)
             br.setConf(content)
-        br2 = CA.binRule(110, len(br.getConf()), 1, CA.binRule.INIT_ZERO)
+        br2 = CA.binRule(ruleNum, len(br.getConf()), 1, CA.binRule.INIT_ZERO)
         br2.setConf(br.getConf())
         assert_arrays_equal(br.getConf(), br2.getConf())
         for i in range(5):
