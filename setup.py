14ce978545dc5a637dc46ce6c9df3db04da8acb0
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-08-21 20:41:14 +0200

    added a setup and tox config.

---
+++ setup.py
@@ -0,0 +1,13 @@
+VERSION = '0.0.1'
+
+from setuptools import setup
+
+setup(
+      name = 'zasim',
+      version = VERSION,
+      author = 'zasim team',
+      description = 'A cellular automaton simulation environment.',
+      keywords = '',
+      url = '',
+      packages = ["zasim"],
+    )
