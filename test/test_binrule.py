eea5ccd78613ca666d0dfaa7d404b331df1e7162
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-08-22 02:39:48 +0200

    kick random argument to test function.

--- test/test_binrule.py
+++ test/test_binrule.py
@@ -1,21 +1,17 @@
 from __future__ import absolute_import
 from zasim import ca
 import numpy as np
-from random import randrange
 from .testutil import *
 
 import pytest
 
 class TestBinRule:
     @pytest.mark.skipif("not ca.HAVE_WEAVE")
-    def test_compare_weave_pure(self, ruleNum, random=True):
+    def test_compare_weave_pure(self, ruleNum):
         """compare the weave version of binRule with the pure python one"""
-        if random:
-            br = ca.binRule(ruleNum, randrange(10,30), 1, ca.binRule.INIT_RAND)
-        else:
-            content = np.array([1,1,1,1,1,0,1,0,1,1,0,0,0,1,1,0,1,0,0,0,1,0,0,0])
-            br = ca.binRule(ruleNum, len(content), 1, ca.binRule.INIT_ZERO)
-            br.setConf(content)
+        content = np.array([1,1,1,1,1,0,1,0,1,1,0,0,0,1,1,0,1,0,0,0,1,0,0,0])
+        br = ca.binRule(ruleNum, len(content), 1, ca.binRule.INIT_ZERO)
+        br.setConf(content)
         br2 = ca.binRule(ruleNum, len(br.getConf()), 1, ca.binRule.INIT_ZERO)
         br2.setConf(br.getConf())
         assert_arrays_equal(br.getConf(), br2.getConf())
