70fba308ae688c88542da050c40aebabf91349bf
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-09-05 14:37:39 +0200

    correct finishing and do everything EVERYTHING!

--- find_all_classes.py
+++ find_all_classes.py
@@ -13,9 +13,9 @@ rule = np.zeros(2 ** 5, dtype=np.dtype("i"))
 start_num = 2 ** 32
 try:
     with open("smaller_results.txt", "r") as old_results:
-        old_results.seek(-200, os.SEEK_END)
-        lastline = old_results.readlines()[-1].strip()
-        numbers = lastline.split(" < ")
+        old_results.seek(-1000, os.SEEK_END)
+        lastdata = old_results.read().replace("\n", "<")
+        numbers = lastdata.split("<")
         numbers = map(int, numbers)
         start_num = max(numbers)
 except IOError:
@@ -23,20 +23,22 @@ except IOError:
 
 
 print "resuming from %d" % (start_num)
-start_num = 2 ** 32
 
-known_bits = multiprocessing.Array("B", int(math.ceil(start_num / 8.0) + 10))
+known_bits = multiprocessing.Array("B", int(math.ceil(start_num / 8.0) + 10), lock=False)
 print "array len is", len(known_bits)
 program_running = multiprocessing.Value('f', 1)
+array_lock = multiprocessing.RLock()
 
-def set_bit(bit):
-    known_bits[bit / 8] |= 1 >> (bit % 8)
+def set_bits(bits):
+    with array_lock:
+        for bit in bits:
+            known_bits[bit / 8] |= 1 >> (bit % 8)
 
 def ask_bit(bit):
-    return (known_bits[bit / 8] & (1 >> (bit % 8))) > 0
+    with array_lock:
+        return (known_bits[bit / 8] & (1 >> (bit % 8))) > 0
 
 def deal_with_range(start, stop, skip, data_queue):
-    counter = 0
     cache_hits = 0
     starttime = time()
 
@@ -53,37 +55,36 @@ def deal_with_range(start, stop, skip, data_queue):
         lower, (route, _), cache = minimize_rule_number(neigh, dav)
         if lower < rule_nr:
             nom = [lower] + cache.keys()
-            for number in nom:
-                set_bit(number)
+            set_bits(nom)
             data_queue.put(nom)
-            counter += len(nom)
 
-    print "range(%d, %d, %d) took %s" % (start, stop, skip, time() - starttime)
+    print "range(%d, %d, %d) took %s - %d cache hits" % (start, stop, skip, time() - starttime, cache_hits)
 
 def master():
-    def write_out_results():
+    def write_out_results(queue):
         with open("find_results.txt", "a") as results:
-            while program_running.value != 0:
+            while program_running.value > 0:
                 try:
-                    data = queue.get_nowait()
-                    data.sort()
+                    data = queue.get(timeout=1)
                     results.write("<".join(map(str,data)))
                     results.write("\n")
                 except Queue.Empty:
-                    pass
+                    print "timed out. program_running.value = ", program_running.value
+
+        print "done writing out results"
 
     queue = multiprocessing.Queue(100)
-    write_proc = multiprocessing.Process(target=write_out_results)
+    write_proc = multiprocessing.Process(target=write_out_results, args=(queue,))
     write_proc.start()
     processes = []
-    start_num = 2 ** 32
     chunksize = 1000
-    for bunch in range(200):
+    for bunch in range(start_num / chunksize):
         while len(processes) >= 3:
             for proc in processes:
                 if not proc.is_alive():
                     processes.remove(proc)
-            sleep(0.5)
+                    proc.join()
+            sleep(0.1)
         proc = multiprocessing.Process(target=deal_with_range,
             args = (start_num - bunch * chunksize,
                     start_num - bunch * chunksize - chunksize,
@@ -91,7 +92,12 @@ def master():
         proc.start()
         processes.append(proc)
         assert write_proc.is_alive()
+    for proc in processes:
+        print "joining a process"
+        proc.join()
     program_running.value = 0
+    print "finished. setting program_running.value = ", program_running.value
+    write_proc.join()
 
 if __name__ == "__main__":
     master()
