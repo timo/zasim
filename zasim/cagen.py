cf54b18a9244838a44e5c45a05d40208156dcb4e
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-09-06 01:14:33 +0200

    correct rule number in automaton name

--- zasim/cagen.py
+++ zasim/cagen.py
@@ -1561,7 +1561,7 @@ class ElementaryCellularAutomatonBase(Computation):
                 "two dimensions"]
 
     def build_name(self, parts):
-        parts.append("calculating rule %d" % (self.rule_nr))
+        parts.append("calculating rule %d" % (self.rule))
 
 class CountBasedComputationBase(Computation):
     """This base class counts the amount of nonzero neighbours excluding the
