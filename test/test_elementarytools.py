535cd64cfab4ceb6ee7904b63a57cdcc1f991661
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-09-13 20:55:58 +0200

    another test for elementarytools

--- test/test_elementarytools.py
+++ test/test_elementarytools.py
@@ -1,10 +1,10 @@
 from zasim import elementarytools
-from zasim.cagen import SimpleNeighbourhood
+from zasim.cagen import SimpleNeighbourhood, VonNeumannNeighbourhood
 
 import pytest
 
 class TestCAGen:
-    def test_flip_horizontal(self):
+    def test_permutations_indexmap(self):
         neigh = SimpleNeighbourhood("lr", [(-1,), (1,)])
         dav = [dict(l=0, r=0, result_value=1),
                dict(l=0, r=1, result_value=2),
@@ -16,4 +16,11 @@ class TestCAGen:
             dict(l=0, r=1, result_value=3),
             dict(l=1, r=0, result_value=2),
             dict(l=1, r=1, result_value=4)]
+        assert res == expected
+
+    def test_flip_horizontal_results_only(self):
+        neigh = SimpleNeighbourhood("lr", [(-1,0), (1,0)])
+        results = [1, 2, 3, 4]
+        res = elementarytools.flip_h(neigh, results)
+        expected = [1, 3, 2, 4]
         assert res == expected
