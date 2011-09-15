41f836ad8b74e9277afdacb911a09d1d5cd8abb2
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-09-15 18:27:30 +0200

    do more different tests, and do them faster.

--- test/test_gui.py
+++ test/test_gui.py
@@ -25,27 +25,56 @@ class TestGui:
         cls.app = QApplication(["test"])
         cls.app.setApplicationName("zasim gui test")
 
-    def test_start_stop_elementary_2d_base2(self):
+    def test_start_stop_elementary(self, size, base, scale):
         other_thread = QThread()
-        sim_obj = cagen.ElementarySimulator((200, 200), rule=99, copy_borders=True, base=2)
+        sim_obj = cagen.ElementarySimulator(size, rule=99, copy_borders=True, base=base)
 
         sim_obj.moveToThread(other_thread)
 
         display = ZasimDisplay(sim_obj)
-        display.set_scale(2)
+        display.set_scale(scale)
 
         other_thread.start()
 
         QTest.mouseClick(display.control.start_button, Qt.LeftButton)
 
-        for execution in seconds(0.5):
+        for execution in seconds(0.1):
             self.app.processEvents()
         assert not display.control.start_button.isVisible()
-        for execution in seconds(0.5):
+        for execution in seconds(0.1):
             self.app.processEvents()
         QTest.mouseClick(display.control.stop_button, Qt.LeftButton)
-        for execution in seconds(0.5):
+        for execution in seconds(0.1):
             self.app.processEvents()
         assert not display.control.stop_button.isVisible()
-        for execution in seconds(0.5):
+        for execution in seconds(0.1):
             self.app.processEvents()
+
+def produce_more(calls, arg, values):
+    """Add, to all `calls`, a call for each `value` for the `arg`, so that
+    all combinations will exist in the returned list.
+
+    If calls is empty, generate a dict for each value as the result."""
+    if not calls:
+        return [{arg: value} for value in values]
+
+    ncalls = []
+    for call in calls:
+        for value in values:
+            new_call = call.copy()
+            new_call[arg] = value
+            ncalls.append(new_call)
+
+    return ncalls
+
+def pytest_generate_tests(metafunc):
+    calls = []
+    if "scale" in metafunc.funcargnames:
+        calls = produce_more(calls, "scale", [1, 4])
+    if "size" in metafunc.funcargnames:
+        calls = produce_more(calls, "size", [(100,), (100, 100)])
+    if "base" in metafunc.funcargnames:
+        calls = produce_more(calls, "base", [2, 3, 5])
+
+    for call in calls:
+        metafunc.addcall(funcargs=call)
