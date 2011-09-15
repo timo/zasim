d2e57de2e02b3454a9f42c37492eca080e7fb52e
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-09-15 17:39:36 +0200

    this is what such a gui test could look like.
    not clear on the multithreading yet.

---
+++ test/test_gui.py
@@ -0,0 +1,44 @@
+from PySide.QtCore import *
+from PySide.QtGui import *
+from PySide.QtTest import *
+
+from zasim.gui.display import ZasimDisplay
+from zasim import cagen
+
+import time
+
+def seconds(num):
+    end = time.time() + num
+    while time.time() < end:
+        yield 1
+
+class TestGui:
+    def setup_class(cls):
+        """craetes a QT app and a socket spy"""
+        cls.app = QApplication(["test"])
+        cls.app.setApplicationName("zasim gui test")
+
+    def test_start_stop_elementary_2d_base2(self):
+        other_thread = QThread()
+        sim_obj = cagen.ElementarySimulator((200, 200), rule=99, copy_borders=True, base=2)
+
+        sim_obj.moveToThread(other_thread)
+
+        display = ZasimDisplay(sim_obj)
+        display.set_scale(2)
+
+        other_thread.start()
+
+        QTest.mouseClick(display.control.start_button, Qt.LeftButton)
+
+        for execution in seconds(0.5):
+            self.app.processEvents()
+        assert not display.control.start_button.isVisible()
+        for execution in seconds(0.5):
+            self.app.processEvents()
+        QTest.mouseClick(display.control.stop_button, Qt.LeftButton)
+        for execution in seconds(0.5):
+            self.app.processEvents()
+        assert not display.control.stop_button.isVisible()
+        for execution in seconds(0.5):
+            self.app.processEvents()
