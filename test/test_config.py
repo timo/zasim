c6fa6c34d1d1afe3d5920c2803167abf70bb3a8d
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-09-26 18:05:29 +0200

    a few tests for config.RandomInitialConfig.

---
+++ test/test_config.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+
+from zasim import config
+
+import pytest
+
+class TestConfig:
+    def test_random_1d(self):
+        a = config.RandomInitialConfiguration()
+        arr = a.generate((1000,))
+        assert not any(arr > 2)
+        assert not any(arr < 0)
+        assert any(arr == 0)
+        assert any(arr == 1)
+        assert len(arr) == 10
+
+        b = config.RandomInitialConfiguration(base=3)
+        brr = b.generate((1000,))
+        assert not any(brr > 3)
+        assert not any(brr < 0)
+        assert any(brr == 0)
+        assert any(brr == 1)
+        assert any(brr == 2)
+
+    def test_random_1d_probabilities(self):
+        a = config.RandomInitialConfiguration(2, 0)
+        arr = a.generate((1000,))
+        assert not any(arr == 0)
+        assert all(arr == 1)
+
+        b = config.RandomInitialConfiguration(2, 1)
+        brr = b.generate((1000,))
+        assert not any(brr == 1)
+        assert all(brr == 0)
+
