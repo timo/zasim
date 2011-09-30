50a3edc1804d48f85aab1463733d3c3e86623644
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-09-30 04:44:52 +0200

    rework the gui

--- zasim/gui/externaledit.py
+++ zasim/gui/externaledit.py
@@ -3,13 +3,14 @@ in external programs. Instantiate one ExternalEditWindow per session (or keep
 it) and call `external_png` or `external_txt` to display the Dialog.
 
 The file will be created as a temporary file, changes to it will automatically
-cause a reload of the config to the simulator and any display updates. An 
+cause a reload of the config to the simulator and any display updates. An
 "import" button is also provided in case the filesystem watcher fails."""
 
 from .displaywidgets import DisplayWidget
 
 from ..external.qt import (QDialog, QHBoxLayout, QVBoxLayout,
         QInputDialog, QLabel, QPushButton, Qt,
+        QDialogButtonBox,
         QFileSystemWatcher)
 
 from ..config import ImageInitialConfiguration, AsciiInitialConfiguration
@@ -36,25 +37,52 @@ class ExternalEditWindow(QDialog):
     def setup_ui(self):
         self.lay = QVBoxLayout(self)
 
-        self.fname_lay = QHBoxLayout()
-
         self.fname_disp = QLabel(parent=self)
         self.fname_disp.setTextInteractionFlags(Qt.TextBrowserInteraction)
-        self.fname_lay.addWidget(self.fname_disp)
+        self.lay.addWidget(self.fname_disp)
 
-        self.import_btn = QPushButton("Import", parent=self)
-        self.import_btn.clicked.connect(self.import_)
-        self.fname_lay.addWidget(self.import_btn)
-        self.lay.addLayout(self.fname_lay)
+        self.btn_box_u = QDialogButtonBox(parent=self)
+        self.btn_box_u.addButton(QDialogButtonBox.Reset)
+        self.btn_box_u.addButton("Update", QDialogButtonBox.AcceptRole)
+        self.btn_box_u.clicked.connect(self.button_clicked)
+
+        self.lay.addWidget(self.btn_box_u)
 
         self.conf_disp = DisplayWidget(self._sim)
         self.conf_disp.set_scale(1)
 
         self.lay.addWidget(self.conf_disp)
 
+        self.btn_box_l = QDialogButtonBox(QDialogButtonBox.Ok |
+                                        QDialogButtonBox.Cancel,
+                                        parent=self)
+        self.btn_box_l.clicked.connect(self.button_clicked)
+        self.lay.addWidget(self.btn_box_l)
+
+    def button_clicked(self, button):
+        for bbox in [self.btn_box_l, self.btn_box_u]:
+            if bbox.buttonRole(button) != QDialogButtonBox.InvalidRole:
+                role = bbox.buttonRole(button)
+                box = bbox
+                break
+
+        if role == QDialogButtonBox.ResetRole:
+            self.reset()
+        elif role == QDialogButtonBox.RejectRole:
+            self.reset()
+            self.reject()
+        elif role == QDialogButtonBox.AcceptRole:
+            if box == self.btn_box_u:
+                self.import_()
+            else:
+                self.accept()
+
     def __external_edit(self, prefix, suffix, importer_class, program):
         assert self.tmpfile is None
         assert self.process is None
+
+        self.original_config = self._sim.get_config()
+
         with NamedTemporaryFile(prefix=prefix, suffix=suffix) as self.tmpfile:
             self.fname_disp.setText(self.tmpfile.name)
             self.exporter.export(self.tmpfile.name)
@@ -64,12 +92,18 @@ class ExternalEditWindow(QDialog):
 
             self.process = Popen(program + [self.tmpfile.name])
 
-            self.exec_()
+            result = self.exec_()
+
+        if result is QDialog.Rejected:
+            self.reset()
 
         self.tmpfile = None
         self.process = None
         del self.watcher
         self.watcher = None
+
+    def reset(self):
+        self._sim.set_config(self.original_config)
 
     def external_png(self, prefix="zasim", suffix=".png"):
         self.exporter = self.conf_disp
