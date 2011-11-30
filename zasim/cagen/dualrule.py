297aa4728234ffc967a3fe3ebdce13bbb04f770a
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-11-30 23:06:17 +0100

    copied over elementary cellular automaton base for dual-rule nondeterminism

---
+++ zasim/cagen/dualrule.py
@@ -0,0 +1,175 @@
+from .bases import Computation
+from .utils import elementary_digits_and_values, rule_nr_to_rule_arr
+
+import numpy as np
+from random import randrange
+import new
+
+class DualRuleCellularAutomaton(Computation):
+    """Infer a 'Gödel numbering' from the used `Neighbourhood` and
+    create a computation that corresponds to the rule'th possible combination
+    of values for the neighbourhood cells.
+
+    This works with any number of dimensions."""
+
+    rule_a = 0
+    """The elementary rule to use with probability `alpha`."""
+    rule_b = 0
+    """The elementary rule to use with probability 1 - `alpha`."""
+    alpha = 0.5
+    """The probability for which to use rule_a rather than rule_b."""
+
+    digits_and_values = []
+    """This list stores a list of dictionaries that for each combination of
+    values for the neighbourhood cells stores the 'result_value', too.
+
+    The result_value field is a tuple of what value to use with alpha and what
+    value to use with 1-alpha probability."""
+
+    def __init__(self, rule_a=None, rule_b=None, alpha=0.5, **kwargs):
+        """Create the computation.
+
+        Supply None as the rule to get a random one."""
+        super(DualRuleCellularAutomaton, self).__init__(**kwargs)
+        self.rule_a = rule_a
+        self.rule_b = rule_b
+        self.alpha  = alpha
+
+    def visit(self):
+        """Get the rule'th cellular automaton for the given neighbourhood.
+
+        First, find out, how many possible combinations there are.
+        That's simply the nuber of cells in the neighbourhood as the exponent
+        of the number of `possible_values`.
+        Then, normalise the neighbourhood cells by sorting their positions
+        first by X, then by Y axis.
+        Finally, create code, that sums up all the values and looks up the
+        target value from the rule lookup array.
+        """
+        super(DualRuleCellularAutomaton, self).visit()
+
+        self.neigh = zip(self.code.neigh.offsets, self.code.neigh.names)
+        self.digits = len(self.neigh)
+
+        self.base = len(self.code.possible_values)
+        # the numbers in the possible values list have to start at 0 and go
+        # all the way up to base-1.
+        assert self.code.possible_values == tuple(range(self.base))
+
+        if self.rule_a is None:
+            self.rule_a = randrange(0, self.base ** (self.base ** self.digits))
+
+        if self.rule_b is None:
+            while self.rule_a != self.rule_b:
+                self.rule_b = randrange(0, self.base ** (self.base ** self.digits))
+
+        if self.rule >= self.base ** (self.base ** self.digits):
+            self.rule = self.rule % (self.base ** (self.base ** self.digits))
+
+        compute_code = ["result = 0;"]
+        compute_py = ["result = 0"]
+        self.code.attrs.append("rule_a")
+        self.code.attrs.append("rule_b")
+        self.code.attrs.append("rule_alpha")
+        self.code.attrs.append("randseed")
+
+        self.code.add_code("localvars",
+                """srand(randseed(0));""")
+        self.code.add_code("after_step",
+                """randseed(0) = rand();""")
+
+        for digit_num, (offset, name) in zip(range(len(self.neigh) - 1, -1, -1), self.neigh):
+            code = "result += %s * %d" % (name, self.base ** digit_num)
+            compute_code.append(code + ";")
+            compute_py.append(code)
+
+        compute_code.append("""
+        if(rand() >= RAND_MAX * rule_alpha) {
+            result = rule_a(result);
+        } else {
+            result = rule_b(result);
+        }""")
+
+        compute_py.append("""# choose which rule to apply
+        if self.random.random() >= rule_alpha:
+            result = self.target.rule_a[int(result)]
+        else:
+            result = self.target.rule_b[int(result)]""")
+
+        self.code.add_code("compute", "\n".join(compute_code))
+        self.code.add_py_hook("compute", "\n".join(compute_py))
+
+    def init_once(self):
+        """Generate the rule lookup array and a pretty printer."""
+        super(DualRuleCellularAutomaton, self).init_once()
+        entries = self.base ** self.digits
+        self.target.rule = np.zeros(entries, np.dtype("i"))
+        rule_a = self.rule_a
+        rule_b = self.rule_b
+
+        self.target.rule_a = np.array(rule_nr_to_rule_arr(rule_a, self.digits, self.base))
+        self.target.rule_b = np.array(rule_nr_to_rule_arr(rule_b, self.digits, self.base))
+
+        # and now do some heavy work to generate a pretty-printer!
+        bbox = self.code.neigh.bounding_box()
+        offsets = self.code.neigh.offsets
+        offset_to_name = dict(self.neigh)
+
+        if len(bbox) == 1:
+            h = 3
+            y_offset = None
+        elif len(bbox) == 2:
+            h = bbox[1][1] - bbox[1][0] + 3
+            y_offset = bbox[1][0]
+        else:
+            # for higher dimensions, just fall back to the dummy pretty-printer
+            return
+        protolines = [[] for i in range(h)]
+        lines = [line[:] for line in protolines]
+        w = bbox[0][1] + 1 - bbox[0][0]
+
+        for y in range(h):
+            for x in range(bbox[0][0], bbox[0][1] + 1):
+                if h == 3 and (x,) in offsets and y == 0:
+                    lines[y].append("%(" + offset_to_name[(x,)]
+                               + ")d")
+                elif h > 3 and (x, y + y_offset) in offsets:
+                    lines[y].append("%(" + offset_to_name[(x, y + y_offset)]
+                               + ")d")
+                else:
+                    lines[y].append(" ")
+            lines[y] = "".join(lines[y]) + "  "
+
+        lines[-1] = ("X".center(w) + "  ").replace("X", "%(result_value)d")
+
+        template = [line[:] for line in lines]
+
+        self.digits_and_values = \
+                elementary_digits_and_values(self.code.neigh, self.base, zip(self.target.rule_a, self.target.rule_b))
+
+        for thedict in self.digits_and_values:
+            a, b = thedict["result_value"]
+            if a != b:
+                thedict["result_value"] = "%s/%s" % (a, b)
+            else:
+                thedict["result_value"] = a
+
+        def pretty_printer(self):
+            lines = [line[:] for line in protolines]
+            for thedict in self.digits_and_values:
+                for line, tmpl_line in zip(lines, template):
+                    line.append(tmpl_line % thedict)
+
+            return "\n".join(["".join(line) for line in lines])
+
+        self.pretty_print = new.instancemethod(pretty_printer, self, self.__class__)
+
+    def pretty_print(self):
+        """This method is generated upon init_once and pretty-prints the rules
+        that this elementary cellular automaton uses for local steps."""
+        return ["pretty printer is available only after the StepFunc "
+                "object has been put together. also: cannot pretty-print "
+                "with neighbourhoods of more than two dimensions"]
+
+    def build_name(self, parts):
+        parts.append("calculating rule %s (%s%%) / %s" % (hex(self.rule_a), self.rule_alpha, hex(self.rule_b)))
