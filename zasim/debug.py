9236ec399125aef270deca063a17b0629532cc27
Timo Paulssen <timonator@perpetuum-immobile.de>
2011-12-08 23:10:26 +0100

    write errors to stderr.

--- zasim/debug.py
+++ zasim/debug.py
@@ -1,8 +1,11 @@
 """This module offers a function to fork a gdb in a terminal and attach it to the
 running process, so that weave stepfuncs and other things may be debugged."""
 
+from __future__ import print_function
+
 import os
 import subprocess as sp
+import sys
 
 def launch_debugger(debugger_binary="gdb", terminal_binary="x-terminal-emulator"):
     os.system("%s -e %s -p %d &" % (terminal_binary, debugger_binary, os.getpid()))
@@ -14,5 +17,5 @@ def indent_c_code(code):
         indent = sp.Popen(["indent", "-kr", "-", "-o", "-"], stdout=sp.PIPE, stdin=sp.PIPE)
         return indent.communicate(code)[0]
     except:
-        print "indent failed"
+        print("indent failed", file=sys.stderr)
         return code
