ba76494c02ed4d53100e7fbae291831a4498a44b
Timo Paulssen <timonator@perpetuum-immobile.de>
2012-05-08 14:18:06 +0200

    the default palette for render_state_array was wrong

--- zasim/display/qt.py
+++ zasim/display/qt.py
@@ -121,7 +121,7 @@ def qimage_to_pngstr(image):
     return str(buf.data())
 
 _last_rendered_state_conf = None
-def render_state_array(states, palette=PALETTE_QC, invert=False, region=None):
+def render_state_array(states, palette=PALETTE_32, invert=False, region=None):
     global _last_rendered_state_conf
     if region:
         x, y, w, h = region
