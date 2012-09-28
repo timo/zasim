b48aa557186902ded0a5041081d483228a6d649d
Timo Paulssen <timonator@perpetuum-immobile.de>
2012-09-28 19:41:54 +0200

    started on "inner" templates

--- zasim/zacformat.py
+++ zasim/zacformat.py
@@ -3,12 +3,13 @@ like text_to_cell outputs."""
 
 import yaml
 import unicodedata
+import itertools
 
 n2c = lambda name: (int(name[1:name.find("l")]), int(name[name.find("l")+1:]))
 n2c.__doc__ = """Turn a name of a subcell into x/y coords"""
 c2n = lambda x, y: "c%dl%d" % (x, y)
 
-def draw_box_template(boxes, w=1):
+def draw_box_template(boxes, t_w=1):
     """
     Create an ascii art template suitable for string interpolation.
 
@@ -114,42 +115,45 @@ def draw_box_template(boxes, w=1):
     for x in range(w+1):
         left_v = bvs if x != 0 else bvd
         for y in range(h+1):
+            tpos = y * 2
             up_h = bhs if y != 0 else bhd
             if (x, y) in boxes:
-                template[y * 2] += corner(x, y) + up_h * (w + 2)
-                template[y * 2 + 1] += left_v + "%%(%s) %ds " % (c2n(x, y), w + 1)
+                template[tpos] += corner(x, y) + up_h * (t_w + 2)
+                template[tpos + 1] += left_v + "%%(%s) %ds " % (c2n(x, y), t_w + 1)
             else:
                 if (x, y-1) in boxes or (x - 1, y) in boxes or (x-1,y-1) in boxes:
-                    template[y * 2] += corner(x, y)
+                    template[tpos] += corner(x, y)
                 else:
-                    template[y * 2] += " "
+                    template[tpos] += " "
                 if (x, y-1) in boxes:
-                    template[y * 2] += up_h * (w + 2)
+                    template[tpos] += up_h * (t_w + 2)
                 else:
-                    template[y * 2] += "  " + " " * w
+                    template[tpos] += "  " + " " * t_w
                 if (x-1,y) in boxes:
-                    template[y * 2 + 1] += left_v
+                    template[tpos + 1] += left_v
                 else:
-                    template[y * 2 + 1] += " "
-                template[y * 2 + 1] += "  " + " " * w
+                    template[tpos + 1] += " "
+                template[tpos + 1] += "  " + " " * t_w
 
     for y in range(h+1):
+        tpos = y * 2
         if (w,y) in boxes or (x, y-1) in boxes:
-            template[y * 2] += corner(w + 1, y)
+            template[tpos] += corner(w + 1, y)
         else:
-            template[y * 2] += " "
+            template[tpos] += " "
         if (w,y) in boxes:
-            template[y * 2 + 1] += bvd
+            template[tpos + 1] += bvd
 
     for x in range(w+1):
+        tpos = h * 2 + 2
         if (x, h) in boxes or (x-1,h) in boxes:
-            template[h * 2 + 2] += corner(x, h + 1)
+            template[tpos] += corner(x, h + 1)
         else:
-            template[h * 2 + 2] += " "
+            template[tpos] += " "
         if (x, h) in boxes:
-            template[h * 2 + 2] += bhd * (w + 2)
+            template[tpos] += bhd * (t_w + 2)
         else:
-            template[h * 2 + 2] += "  " + " " * w
+            template[tpos] += "  " + " " * t_w
 
     if (w, h) in boxes:
         template[h * 2 + 2] += corner(w+1,h+1)
@@ -161,7 +165,54 @@ def draw_tiled_box_template(boxes, w=1, twodim=True):
     center for a lattice of box-templates or - if twodim is False - for the
     left and right end and the body of a line of box-templates."""
 
-    # TODO: actually make the border ones, too.
+    originalboxes = boxes[:]
+
+    neighbours = itertools.product([-1,0,1], [-1, 0, 1])
+
+    content_width = w
+
+    w = max([t[0] for t in boxes])
+    h = max([t[1] for t in boxes])
+
+    # determine wether the corners have boxes next to them.
+    luc = [(-1,-1)] if (w,h) in boxes else []
+    ruc = [( w,-1)] if (0,h) in boxes else []
+    ldc = [(-1, h)] if (w,0) in boxes else []
+    rdc = [( w, h)] if (0,0) in boxes else []
+
+    def warp(src,dst,axis):
+        res = []
+        for box in boxes:
+            if box[axis] == src:
+                if axis == 0:
+                    res.append((dst, box[1]))
+                else:
+                    res.append((box[0], dst))
+        return res
+
+    # copy border boxes around
+    lb = warp(0, w+1, 0)
+    rb = warp(w,  -1, 0)
+
+    ub = warp(h,  -1, 1)
+    db = warp(0, h+1, 1)
+
+    result_template = {}
+    for x, y in neighbours:
+        fixed = originalboxes[:]
+        if (x-1, y-1) in neighbours: fixed.extend(luc)
+        if (x+1, y-1) in neighbours: fixed.extend(ruc)
+        if (x-1, y+1) in neighbours: fixed.extend(ldc)
+        if (x+1, y+1) in neighbours: fixed.extend(rdc)
+
+        if (x-1, y  ) in neighbours: fixed.extend(lb)
+        if (x+1, y  ) in neighbours: fixed.extend(rb)
+        if (x,   y-1) in neighbours: fixed.extend(ub)
+        if (x,   y+1) in neighbours: fixed.extend(db)
+
+        result_template[(x,y)] = draw_box_template(fixed, content_width)
+
+    return result_template
 
 class ZacConsoleDisplay(object):
     def __init__(self, simulator, sets, strings, connect=True, auto_output=True):
