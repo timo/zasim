271eb4ad4e7ab824df513aee8ec98a4eacb8ed8c
Timo Paulssen <timonator@perpetuum-immobile.de>
2012-10-30 17:37:52 +0100

    rough draft of what signal code could look like.

--- zasim/cagen/signal.py
+++ zasim/cagen/signal.py
@@ -14,5 +14,26 @@ Setting the out_signal and out_signal_dir variables will send a new signal."""
 from .bases import Computation
 
 class SignalService(Computation):
-    pass
+    def __init__(self, signal_field="signal", direction_field="sig_dir"):
+        self.signal_field = signal_field
+        self.direction_field = direction_field
 
+
+signal = None
+signal_dir = None
+signal_received = False
+
+out_signal = None
+out_signal_dir = None
+for idx, (n, rn) in enumerate(zip([neighbours_here], [reverse_neighbours_here])):
+    if n_sig_dir == rn:
+        if signal_received:
+            raise Exception("trying to read from %s and %s" % (signal_dir, idx))
+        signal = n_signal
+        signal_dir = idx
+        signal_received = True
+
+[...]
+
+result_signal = out_signal or 0
+result_signal_dir = out_signal or -1
