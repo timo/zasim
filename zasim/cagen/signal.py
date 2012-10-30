6a72b7f1e77ed336d7a19d33a735355dd5a5d17f
Timo Paulssen <timonator@perpetuum-immobile.de>
2012-10-30 22:01:30 +0100

    initial implementation of signal service in python

--- zasim/cagen/signal.py
+++ zasim/cagen/signal.py
@@ -12,28 +12,55 @@ multiple signals addressed to the cell, an Exception is raised.
 Setting the out_signal and out_signal_dir variables will send a new signal."""
 
 from .bases import Computation
+from .compatibility import no_weave_code
 
 class SignalService(Computation):
+    provides_features = [no_weave_code]
+
     def __init__(self, signal_field="signal", direction_field="sig_dir"):
         self.signal_field = signal_field
         self.direction_field = direction_field
 
+    def visit(self):
+        self.code.add_py_code("compute",
+                """# signal service
+                signal = None
+                signal_dir = None
+                signal_received = False
 
-signal = None
-signal_dir = None
-signal_received = False
+                out_signal = None
+                out_signal_dir = None""")
 
-out_signal = None
-out_signal_dir = None
-for idx, (n, rn) in enumerate(zip([neighbours_here], [reverse_neighbours_here])):
-    if n_sig_dir == rn:
-        if signal_received:
-            raise Exception("trying to read from %s and %s" % (signal_dir, idx))
-        signal = n_signal
-        signal_dir = idx
-        signal_received = True
+        lines = [""]
+        put_else = False
+        for idx, (neigh, revneighidx) in enumerate(zip(
+                self.code.neigh.names,
+                map(self.code.neigh.reverse, self.code.neigh.names))):
+            revneighname = self.code.neigh.names[revneighidx]
+            part = ""
+            if put_else: part = "el"
+            else:    put_else = True
 
-[...]
+            part += "if %s_%s == %d: # %s" % (
+                        neigh,
+                           self.direction_field,
+                                 revneighidx,
+                                       revneighname)
+            if put_else:
+                part += "    if signal_received: raise Exception('received signal from multiple sides!')"
+            part += "    signal_received = True"
+            part += "    signal = %s_%s" % (neigh, self.signal_field)
+            part += "    signal_dir = %d # %s" % (revneighidx, revneighname)
 
-result_signal = out_signal or 0
-result_signal_dir = out_signal or -1
+            lines.append(part)
+
+        self.code.add_py_code("compute",
+                "\n".join(lines))
+        self.code.add_py_code("compute", "# end of signal service")
+
+        self.code.add_py_code("compute_reversed",
+                """# signal service
+                result_%s = out_signal
+                result_%s = out_signal_dir""" % (
+                    self.signal_field, self.direction_field))
+
